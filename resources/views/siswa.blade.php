3d4674e85ad25c68a9daa6fe790779f734786a06
idn-fahmifn <fahmifn.idn@gmail.com>
2024-10-18 10:46:43 +0700

    update-18-oct

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Siswa</title>
+</head>
+<body>
+    <h1>Ini adalah halaman siswa,</h1>
+    <h3>SMP BQ Islamic Boarding School</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ini untuk menampilkan halaman
+
+Route::get('siswa', function(){
+    return view('siswa');
+});
+// ini untuk menampilkan respon dan parameter
+Route::get('tentang-siswa/{nama}', function($nama){
+    return "Ini adalah kisah kesuksesan ".$nama;
+});
+// menampilkan parameter di halaman
+Route::get('detail/{nama}', function($nama){
+    $alamat = 'Jalan Pinus';
+    return view('detail', compact('nama', 'alamat'));
+});
+
+// halaman form
+Route::get('form-siswa', function(){
+   return view('form'); 
 });
